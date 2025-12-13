e9c53a37faa8bbb3974863d38e9493f3cc50209c
mariByr <byramarichka@gmail.com>
2025-12-14 01:12:44 +0200

    HW1 task 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 
 import './App.css'
 
+
+const coursesTitleArray: string[] = [
+  'JavaScript Complex',
+  'Java Complex',
+  'Python Complex',
+  'QA Complex',
+  'Fullstack',
+  'Frontend'
+];
+
 function App() {
 
 
   return (
     <>
+<ul>
+  {
+    coursesTitleArray.map((value,index) => <li key={index}>{value}</li>)
+  }
+</ul>
 
-
-
+      <h1 className="text-red-500 text-3xl font-bold">
+        Tailwind працює 🚀
+      </h1>
     </>
   )
 }
